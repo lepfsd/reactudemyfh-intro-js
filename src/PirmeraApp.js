7c5f10c98f2306bc5fda6c010a7dd33f69d0b9c5
lepfsd <lepfsd00@gmail.com>
2021-08-24 10:59:05 -0400

    hello world react

---
+++ src/PirmeraApp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PrimeraApp = () => {
+    return <h1>Hello world, react!</h1>
+}
+
+export default PrimeraApp
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-const getImagen = async () => {
-    try {
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PrimeraApp from './PirmeraApp';
+import './index.css'
 
-        const apiKey = 'frfEF9kjVH5vLsDRcMahR35UtRRHgfMN'
-        const resp = await fetch(`api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
-        const {data} =  await resp
-        const { url } = data.images.original
-        const img = document.createElement('img')
-        img.src = url
-        document.body.append(img)
-    }
-    catch(error) {
-        console.log(error)
-    }
-}
-
-getImagen()
+ReactDOM.render(
+    <PrimeraApp />,
+    document.getElementById('root')
+);
